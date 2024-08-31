e2fc66ad291d236ec9cf09185b75279c798032f4
Kaustubh Ratwadkar <145931905+149189@users.noreply.github.com>
2024-08-31 15:09:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import './styles.css';
-import App from './App';
+import App from './app';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
