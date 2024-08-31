2c8b61a53c5578efd1a6501fc183cd3ca44a3745
Kaustubh Ratwadkar <145931905+149189@users.noreply.github.com>
2024-08-31 23:17:01 +0530

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Equipment from './components/Equipment';
@@ -8,15 +8,15 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact component={Home} />
-          <Route path="/equipment" component={Equipment} />
-          <Route path="/login" component={Login} />
-        </Routes>
-        <Footer />
-      </div>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/equipment" element={<Equipment />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Use the new client API
 import './styles.css';
-import App from './App';
+import App from './app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root
+root.render(<App />); // Render the app within the root
